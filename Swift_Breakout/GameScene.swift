a97fab888a7d9396131e6f98bfffd93423ac62cf
hideki.ichiki <hideki.ichiki@mkt-apps.com>
2018-01-24 14:17:12 +0900

    初期化処理の追記

--- Swift_Breakout/GameScene.swift
+++ Swift_Breakout/GameScene.swift
@@ -9,64 +9,70 @@
 import SpriteKit
 import GameplayKit
 
-class GameScene: SKScene {
+class GameScene: SKScene, SKPhysicsContactDelegate {
     
-    private var label : SKLabelNode?
-    private var spinnyNode : SKShapeNode?
+    let scoreDefault: UInt = 0
+    let ballLifeDefault: UInt = 3
+    let scoreMagnificationDefault: Int = 3
     
-    override func didMove(to view: SKView) {
+    var score: UInt?
+    var ballLife: UInt?
+    var scoreMagnification: Int?
+    var isLoop: Bool = false
+    var isRebone: Bool = false
+    var isDead: Bool = false
+    var showString: SKLabelNode?
+    var deadZone: SKSpriteNode?
+    
+    override init(size: CGSize) {
         
-        // Get label node from scene and store it for use later
-        self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
-        if let label = self.label {
-            label.alpha = 0.0
-            label.run(SKAction.fadeIn(withDuration: 2.0))
-        }
+        super.init(size: size)
+        self.backgroundColor = UIColor.black
+        self.settingPhysics()
+        self.settingLife()
+    }
+    
+    private func settingPhysics() {
         
-        // Create shape node to use during mouse interaction
-        let w = (self.size.width + self.size.height) * 0.05
-        self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
+        self.isLoop = false
+        self.isRebone = false
+        self.isDead = false
+        self.physicsBody = SKPhysicsBody.init(edgeLoopFrom: self.frame)
+        self.physicsWorld.gravity = CGVector.init(dx: 0.0, dy: 0.0)
+        self.physicsWorld.contactDelegate = self
+        self.physicsBody?.collisionBitMask = Category().wall
+    }
+    
+    private func settingLife() {
         
-        if let spinnyNode = self.spinnyNode {
-            spinnyNode.lineWidth = 2.5
-            
-            spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(Double.pi), duration: 1)))
-            spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5),
-                                              SKAction.fadeOut(withDuration: 0.5),
-                                              SKAction.removeFromParent()]))
+        self.score = self.scoreDefault
+        self.scoreMagnification = self.scoreMagnificationDefault
+        self.ballLife = self.ballLifeDefault
+        
+        self.showString = SKLabelNode()
+        if let showString = self.showString {
+            showString.text = ("Life:\(String(describing: self.ballLife)) Score:\(String(describing: self.score))")
+            showString.position = CGPoint(x:self.size.width/2, y:self.size.height - showString.frame.height - 20)
         }
     }
     
+    required init?(coder aDecoder: NSCoder) {
+        fatalError()
+    }
     
+    override func didMove(to view: SKView) {
+    }
+
     func touchDown(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.green
-            self.addChild(n)
-        }
     }
     
     func touchMoved(toPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.blue
-            self.addChild(n)
-        }
     }
     
     func touchUp(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.red
-            self.addChild(n)
-        }
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if let label = self.label {
-            label.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
-        }
-        
         for t in touches { self.touchDown(atPoint: t.location(in: self)) }
     }
     
@@ -82,8 +88,6 @@ class GameScene: SKScene {
         for t in touches { self.touchUp(atPoint: t.location(in: self)) }
     }
     
-    
     override func update(_ currentTime: TimeInterval) {
-        // Called before each frame is rendered
     }
 }
